d247eb99373c2da687404d88f642101f6b38d605
Antonio <antonioscalisi0397@gmail.com>
2019-10-31 09:24:41 +0100

    modica di main in branch3

--- MioTest2/src/Main.java
+++ MioTest2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2 commit");
+		System.out.println("2 commit on branch3");
 	}
 
 }
